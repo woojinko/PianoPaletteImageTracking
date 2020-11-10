cf71feeef30cdd33c99e9476541ab6ebeabe06cc
Umang Srivastav <usrivastav10@gmail.com>
2020-11-10 15:22:45 -0800

    Added plane detection, point cloud and cursor

---
+++ Assets/Scripts/Components/GeneralConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GeneralConfiguration : MonoBehaviour
+{
+    [Header("Visual Items")]
+
+    [SerializeField]
+    [Tooltip("Prefab : Used to visualise where an object will be placed on planes - instanced by 'PlacementCursorManager'")]
+    public GameObject placementCursorPrefab;
+}
---
+++ Assets/Scripts/Managers/PlacementCursorManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementCursorManager : MonoBehaviour
+{
+    [Header("Visual Items")]
+
+    [SerializeField]
+    [Tooltip("Prefab : Used to visualise where an object will be placed on planes - instanced by 'PlacementCursorManager'")]
+    public GameObject placementCursorPrefab;
+
+
+    [SerializeField] 
+    public Pose placementCursorPose; // used as a way to expose the placement and position of the spot where they raycast touches a surface
+
+    [SerializeField]
+    public bool placementCursorIsSurface = false;
+
+    private ARRaycastManager arRaycastManager;
+
+    private GameObject placementCursor;
+
+
+    void Awake()
+    {
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+        placementCursor = Instantiate(placementCursorPrefab) as GameObject; 
+    }
+
+    void Update()
+    {
+        UpdateCursorPose();
+        UpdateCursorIndicator();
+    }
+
+    private void UpdateCursorPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var arRaycastHits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, arRaycastHits, TrackableType.Planes);
+        placementCursorIsSurface = arRaycastHits.Count > 0;
+        if (placementCursorIsSurface)
+        {
+            placementCursorPose = arRaycastHits[0].pose;
+        }
+    }
+
+    private void UpdateCursorIndicator()
+    {
+        if (placementCursorIsSurface)
+        {
+            placementCursor.SetActive(true);
+            placementCursor.transform.SetPositionAndRotation(placementCursorPose.position, placementCursorPose.rotation);
+        }
+        else
+        {
+            placementCursor.SetActive(false);
+        }
+    }
+}
